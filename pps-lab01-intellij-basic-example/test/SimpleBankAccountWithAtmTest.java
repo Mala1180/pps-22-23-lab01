89dadf9ed5d33fe8620ebe2c0ce1a96c9458ca75
Mattia Matteini <matteinimattia@gmail.com>
2023-02-27 12:02:24 +0100

    feat: implemented wrong use case test for deposit and withdraw

--- pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -1,3 +1,5 @@
+import lab01.example.exception.IllegalAmountException;
+import lab01.example.exception.IllegalUserException;
 import lab01.example.model.AccountHolder;
 import lab01.example.model.BankAccount;
 import lab01.example.model.SimpleBankAccountWithAtm;
@@ -30,10 +32,27 @@ class SimpleBankAccountWithAtmTest {
     }
 
     @Test
+    void testWrongDepositWithATM() {
+        assertThrows(IllegalUserException.class, () -> bankAccount.deposit(2, 50));
+    }
+
+    @Test
     void testWithdrawWithATM() {
         bankAccount.deposit(accountHolder.getId(), 100);
         bankAccount.withdraw(accountHolder.getId(), 70);
         assertEquals(30 - 2 * SimpleBankAccountWithAtm.ATM_FEE, bankAccount.getBalance());
+    }
+
+    @Test
+    void testWrongUserWithdraw() {
+        bankAccount.deposit(accountHolder.getId(), 100);
+        assertThrows(IllegalUserException.class, () -> bankAccount.withdraw(2, 70));
+    }
+
+    @Test
+    void testWrongAmountWithdraw() {
+        bankAccount.deposit(accountHolder.getId(), 100);
+        assertThrows(IllegalAmountException.class, () -> bankAccount.withdraw(accountHolder.getId(), 120));
     }
 
 }
