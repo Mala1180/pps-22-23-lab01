daa84e9843ac78d23a7efebd0dfb35b5f8c4c83a
Mattia Matteini <matteinimattia@gmail.com>
2023-02-27 11:21:21 +0100

    wip: started banck account exercise, setup new classes

---
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -0,0 +1,19 @@
+import lab01.example.model.AccountHolder;
+import lab01.example.model.BankAccount;
+import lab01.example.model.SimpleBankAccountWithAtm;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleBankAccountWithAtmTest {
+
+    private AccountHolder accountHolder;
+    private BankAccount bankAccount;
+
+
+    @BeforeEach
+    void beforeEach(){
+        accountHolder = new AccountHolder("Mario", "Rossi", 1);
+        bankAccount = new SimpleBankAccountWithAtm(accountHolder, 0);
+    }
+}
