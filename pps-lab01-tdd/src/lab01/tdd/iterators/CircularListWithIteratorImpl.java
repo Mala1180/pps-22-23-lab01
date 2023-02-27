4ee98cc6ef4b525f6abc424dd621cfc3e79ce5b1
Mattia Matteini <matteinimattia@gmail.com>
2023-02-27 22:45:57 +0100

    wip: implemented and tested forward and backward iterators

--- pps-lab01-tdd/src/lab01/tdd/iterators/CircularListWithIteratorImpl.java
+++ pps-lab01-tdd/src/lab01/tdd/iterators/CircularListWithIteratorImpl.java
@@ -3,6 +3,9 @@ package lab01.tdd.iterators;
 import lab01.tdd.CircularList;
 
 import java.util.Iterator;
+import java.util.function.IntSupplier;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 public class CircularListWithIteratorImpl implements CircularListWithIterators {
 
@@ -14,11 +17,17 @@ public class CircularListWithIteratorImpl implements CircularListWithIterators {
 
     @Override
     public Iterator<Integer> forwardIterator() {
-        return null;
+        return this.iterator(() -> this.circularList.next().get());
     }
 
     @Override
     public Iterator<Integer> backwardIterator() {
-        return null;
+        return this.iterator(() -> this.circularList.previous().get());
+    }
+
+    private Iterator<Integer> iterator(IntSupplier supplier) {
+        return circularList.size() == 0 ?
+                IntStream.empty().iterator() :
+                IntStream.generate(supplier).iterator();
     }
 }
--- pps-lab01-tdd/test/lab01/tdd/iterators/CircularListWithIteratorsTest.java
+++ pps-lab01-tdd/test/lab01/tdd/iterators/CircularListWithIteratorsTest.java
@@ -3,6 +3,11 @@ package lab01.tdd.iterators;
 import lab01.tdd.CircularList;
 import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class CircularListWithIteratorsTest {
 
@@ -17,6 +22,30 @@ class CircularListWithIteratorsTest {
         circularList.add(3);
         circularList.add(4);
         circularListWithIterators = new CircularListWithIteratorImpl(circularList);
+    }
+
+    @Test
+    void testForwardIterator() {
+        Iterator<Integer> iterator = circularListWithIterators.forwardIterator();
+        assertEquals(1, iterator.next());
+        assertEquals(2, iterator.next());
+        assertEquals(3, iterator.next());
+        assertEquals(4, iterator.next());
+        assertEquals(1, iterator.next());
+        assertEquals(2, iterator.next());
+        assertTrue(iterator.hasNext());
+    }
+
+    @Test
+    void testBackwardIterator() {
+        Iterator<Integer> iterator = circularListWithIterators.backwardIterator();
+        assertEquals(4, iterator.next());
+        assertEquals(3, iterator.next());
+        assertEquals(2, iterator.next());
+        assertEquals(1, iterator.next());
+        assertEquals(4, iterator.next());
+        assertEquals(3, iterator.next());
+        assertTrue(iterator.hasNext());
     }
 
 }
