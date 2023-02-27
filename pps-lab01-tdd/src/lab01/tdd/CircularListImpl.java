420ec3afbc3dc2504a189ad7f07baa088e405091
Mattia Matteini <matteinimattia@gmail.com>
2023-02-27 11:03:27 +0100

    wip: implemented getNext and getPrevious

--- pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
+++ pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
@@ -1,13 +1,14 @@
 package lab01.tdd;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
 public class CircularListImpl implements CircularList {
 
     private final List<Integer> list = new ArrayList<>();
-
+    private int currentElementIndex = -1;
 
     @Override
     public void add(int element) {
@@ -26,12 +27,26 @@ public class CircularListImpl implements CircularList {
 
     @Override
     public Optional<Integer> next() {
-        return Optional.empty();
+        if (this.list.isEmpty()) {
+            return Optional.empty();
+        }
+        currentElementIndex = currentElementIndex + 1;
+        if (currentElementIndex == this.list.size()) {
+            currentElementIndex = 0;
+        }
+        return Optional.of(this.list.get(currentElementIndex));
     }
 
     @Override
     public Optional<Integer> previous() {
-        return Optional.empty();
+        if (this.list.isEmpty()) {
+            return Optional.empty();
+        }
+        currentElementIndex = currentElementIndex - 1;
+        if (currentElementIndex < 0) {
+            currentElementIndex = this.list.size() - 1;
+        }
+        return Optional.of(this.list.get(currentElementIndex));
     }
 
     @Override
--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -3,6 +3,8 @@ import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -29,5 +31,48 @@ public class CircularListTest {
         circularList.add(2);
         circularList.add(3);
         assertEquals(3, circularList.size());
+    }
+
+    @Test
+    void testGetNext() {
+        circularList.add(1);
+        circularList.add(2);
+        assertEquals(1, circularList.next().get());
+        assertEquals(2, circularList.next().get());
+        circularList.add(3);
+        assertEquals(3, circularList.next().get());
+    }
+
+    @Test
+    void testGetNextIfListIsEmpty() {
+        assertEquals(Optional.empty(), circularList.next());
+    }
+
+    @Test
+    void testGetNextAndGetPrevious() {
+        circularList.add(1);
+        circularList.add(2);
+        circularList.add(3);
+        assertEquals(1, circularList.next().get());
+        assertEquals(2, circularList.next().get());
+        assertEquals(3, circularList.next().get());
+        assertEquals(2, circularList.previous().get());
+        assertEquals(1, circularList.previous().get());
+    }
+
+    @Test
+    void testGetNextCircular() {
+        circularList.add(1);
+        circularList.add(2);
+        assertEquals(1, circularList.next().get());
+        assertEquals(2, circularList.next().get());
+        assertEquals(1, circularList.next().get());
+    }
+
+    @Test
+    void testGetPreviousCircular() {
+        circularList.add(1);
+        circularList.add(2);
+        assertEquals(2, circularList.previous().get());
     }
 }
